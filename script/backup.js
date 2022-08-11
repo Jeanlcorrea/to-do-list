13af23e01f67d921a8656ba2f12aceb1cb96b3bb
Jean Correa <102528406+Jeanlcorrea@users.noreply.github.com>
2022-08-11 19:23:47 -0300

    Update backup.js

--- script/backup.js
+++ script/backup.js
@@ -10,7 +10,7 @@ document.getElementById('button_add').onclick = function() {
 
     var numero_de_tarefas = document.querySelectorAll('#deletar');
     
-    for (let index = 0; index < numero_de_tarefas.length; index++) 
+    for (let index = 0 index < numero_de_tarefas.length; index++) 
     numero_de_tarefas[index].onclick = function() {
         this.parentNode.remove()
     }
@@ -23,14 +23,14 @@ document.getElementById('button_add').onclick = function() {
     if (text_input.value == 0){
         window.alert='Digite sua tarefa';
     }else {
-        document.getElementById('novas_tarefas').innerHTML += new_task;
+        document.getElementById('novas_tarefas').inrHTML += new_task;
     }
 
     var numero_e_tarefas = document.querySelectorAll('#deletar');
     
     for (let index = 0; index < numero_de_tarefas.length; index++) 
     numero_de_tarefas[index].onclick = function() {
-        this.parentode.remove()
+        this.parentNode.remove()
     }
 }
 
